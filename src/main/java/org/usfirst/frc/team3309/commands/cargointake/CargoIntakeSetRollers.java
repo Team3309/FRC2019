755b72a3a47faeaf1131d665902bcb91606ddba2
Chase Blagden <chase.blagden@gmail.com>
2019-01-30 18:49:00 -0800

    refactored commands

---
+++ src/main/java/org/usfirst/frc/team3309/commands/cargointake/CargoIntakeSetRollers.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team3309.commands.cargointake;
+
+import org.usfirst.frc.team3309.Robot;
+import org.usfirst.frc.team4322.commandv2.Command;
+
+public class CargoIntakeSetRollers extends Command {
+
+    private double power;
+
+    public CargoIntakeSetRollers(double power) {
+        require(Robot.cargoIntake);
+        this.power = power;
+    }
+
+    @Override
+    protected void execute() {
+        Robot.cargoIntake.setPower(power);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+}
--- src/main/java/org/usfirst/frc/team3309/commands/panelintake/PanelIntakeStopRollers.java
+++ src/main/java/org/usfirst/frc/team3309/commands/panelintake/PanelIntakeStopRollers.java
@@ -3,23 +3,19 @@ package org.usfirst.frc.team3309.commands.panelintake;
 import org.usfirst.frc.team3309.Robot;
 import org.usfirst.frc.team4322.commandv2.Command;
 
-public class PanelIntakeSetRollers extends Command {
+public class PanelIntakeStopRollers extends Command {
 
-    private double power;
-
-    public PanelIntakeSetRollers(double power) {
+    public PanelIntakeStopRollers() {
         require(Robot.panelIntake);
-        this.power = power;
     }
 
     @Override
-    protected void initialize() {
-        Robot.panelIntake.setPower(power);
+    public void execute() {
+        Robot.panelIntake.setPower(0.0);
     }
 
     @Override
     protected boolean isFinished() {
-        return true;
+        return false;
     }
-
 }
