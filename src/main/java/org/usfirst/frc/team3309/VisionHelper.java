8279af8640aba99feaff8eaf8b62389c094444f2
Chase Blagden <chase.blagden@gmail.com>
2019-03-07 20:56:07 -0800

    added throttle controller to vision helper

--- src/main/java/org/usfirst/frc/team3309/VisionHelper.java
+++ src/main/java/org/usfirst/frc/team3309/VisionHelper.java
@@ -1,14 +1,32 @@
 package org.usfirst.frc.team3309;
 
+import org.usfirst.frc.team3309.lib.Limelight;
 import org.usfirst.frc.team3309.lib.PIDController;
+import org.usfirst.frc.team3309.lib.util.DriveSignal;
 import org.usfirst.frc.team3309.subsystems.Vision;
 
 public class VisionHelper {
 
-    private static Vision.Limelight limelight = Vision.panelLimelight;
+    private static Limelight limelight = Vision.panelLimelight;
+
     private static PIDController turnController = new PIDController("turn", 0.07, 0.00001, 0.0);
+    private static PIDController throttleController = new PIDController("throttle", 0.0, 0.0, 0.0);
 
     private static final boolean isDashboard = false;
+
+    private static final double PANEL_HEIGHT = 0.0;
+    private static final double CAMERA_HEIGHT = 0.0;
+
+    public static DriveSignal getDriveSignal() {
+        double linearPower = getThrottleCorrection();
+        double angularPower = getTurnCorrection();
+        return new DriveSignal(linearPower + angularPower,
+                linearPower - angularPower);
+    }
+
+    public static double getThrottleCorrection() {
+        return throttleController.update(getDist());
+    }
 
     public static double getTurnCorrection() {
         if (isDashboard) {
@@ -25,6 +43,10 @@ public class VisionHelper {
             angularPower = turnController.update(limelightAngle);
         }
         return angularPower;
+    }
+
+    public static double getDist() {
+        return Math.abs(PANEL_HEIGHT - CAMERA_HEIGHT) / Math.tan(limelight.getTy());
     }
 
 }
--- src/main/java/org/usfirst/frc/team3309/subsystems/Vision.java
+++ src/main/java/org/usfirst/frc/team3309/subsystems/Vision.java
@@ -1,21 +1,15 @@
 package org.usfirst.frc.team3309.subsystems;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import org.usfirst.frc.team3309.Robot;
+import org.usfirst.frc.team3309.lib.Limelight;
 import org.usfirst.frc.team4322.commandv2.Subsystem;
 
 public class Vision extends Subsystem {
 
-    public static Limelight cargoLimelight;
     public static Limelight panelLimelight;
 
     public Vision() {
-        cargoLimelight = new Limelight("limelight-cargo");
         panelLimelight = new Limelight("limelight-panel");
-        cargoLimelight.setLed(Limelight.LEDMode.Off);
         panelLimelight.setLed(Limelight.LEDMode.Off);
-        cargoLimelight.setCamMode(Limelight.CamMode.DriverCamera);
         panelLimelight.setCamMode(Limelight.CamMode.DriverCamera);
     }
 
@@ -27,75 +21,5 @@ public class Vision extends Subsystem {
         limelight.setLed(mode);
     }
 
-    public static class Limelight {
-
-        private NetworkTable table;
-
-        public Limelight(String tablename) {
-            table = NetworkTableInstance.getDefault().getTable(tablename);
-        }
-
-        public boolean hasTarget() {
-            return get("tv") == 1.0;
-        }
-
-        public double getTx() {
-            return get("tx");
-        }
-
-        public double getSkew() {
-            return get("ts");
-        }
-
-        public double getArea() {
-            return get("ta");
-        }
-
-        public double get(String entryName) {
-            return table.getEntry(entryName).getDouble(0.0);
-        }
-
-        public void setPipeline(int pipeline) {
-            table.getEntry("pipeline").setDouble(pipeline);
-        }
-
-        public int getPipeline() {
-            return (int) table.getEntry("pipeline").getDouble(0.0);
-        }
-
-        public void setLed(LEDMode mode) {
-            table.getEntry("ledMode").setDouble(mode.value);
-        }
-
-        public void setCamMode(CamMode camMode) {
-            if (camMode == CamMode.VisionProcessor) {
-                table.getEntry("camMode").setDouble(0.0);
-            } else if (camMode == CamMode.DriverCamera) {
-                table.getEntry("camMode").setDouble(1.0);
-            }
-        }
-
-        public enum LEDMode {
-            Off(1),
-            On(3);
-
-            private int value;
-
-            LEDMode(int value) {
-                this.value = value;
-            }
-
-            public int get() {
-                return value;
-            }
-
-        }
-
-        public enum CamMode {
-            VisionProcessor,
-            DriverCamera
-        }
-
-    }
 
 }
