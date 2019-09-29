9526d8303bbe514f3bdfe3b9482c3d2c1e02e39e
Doug Kavner <dmkavner@yahoo.com>
2019-09-29 11:55:08 -0700

    Chezy start of elims - restore match 53 placement bias

--- src/main/java/org/usfirst/frc/team3309/Constants.java
+++ src/main/java/org/usfirst/frc/team3309/Constants.java
@@ -151,7 +151,7 @@ public class Constants {
     public static final double kPanelLimelightInchesX = -4.5;  // lateral off-center
     public static final double kPanelLimelightPlacementInchesZ = 26.5;  // behind placement point
     public static final double kPanelLimelightRotationCenterInchesZ = 4.0;  // behind bot rotational center
-    public static final double kPanelHolderBiasInchesX = 0.35;  // negative value biases placement to the right
+    public static final double kPanelHolderBiasInchesX = 0.85;  // negative value biases placement to the right
 
     // PDP channels
     public static final int kPdpChannelDriveLeft1 = 0;
