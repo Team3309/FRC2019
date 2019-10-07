e2017b9e3b4d26c84134a92f887fee14940cb9fc
Doug Kavner <dmkavner@yahoo.com>
2019-10-07 15:42:35 -0700

    Tune drive sensitivity + elevator tolerance

--- src/main/java/org/usfirst/frc/team3309/lib/util/CheesyDriveHelper.java
+++ src/main/java/org/usfirst/frc/team3309/lib/util/CheesyDriveHelper.java
@@ -19,8 +19,8 @@ public class CheesyDriveHelper {
 
     private static final double kHighNegInertiaScalar = 4.0;
 
-    private static final double kHighSensitivity = 1.2;
-    private static final double kLowSensitivity = 1.2;
+    private static final double kHighSensitivity = .9;
+    private static final double kLowSensitivity = .9;
 
     private static final double kLowNegInertiaThreshold = 0.65;
     private static final double kLowNegInertiaTurnScalar = 1.5;
