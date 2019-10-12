5dbe78e1912500bfbb3c15a10bfb38fec2ac10c2
Doug Kavner <dmkavner@yahoo.com>
2019-10-12 10:23:51 -0700

    Added pipeline for bright lights. Slow down placement approach.

--- src/main/java/org/usfirst/frc/team3309/commands/PlacePanel.kt
+++ src/main/java/org/usfirst/frc/team3309/commands/PlacePanel.kt
@@ -9,6 +9,7 @@ fun PlacePanel(): Command {
     return group {
         sequential {
             +PanelHolderActuate(PanelHolder.PanelHolderPosition.TelescopeForwards)
+            +WaitCommand(0.2)
         }
     }
 }
