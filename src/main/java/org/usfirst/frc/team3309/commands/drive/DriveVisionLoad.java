88931742e33467ce6f9d93f11a9dac52f231dc15
Joshua Badzey <themaninthesnowboots@gmail.com>
2019-12-28 15:12:18 -0800

    Preparation for DriveVisionLoad tweaking.

--- src/main/java/org/usfirst/frc/team3309/commands/drive/DriveVisionLoad.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/DriveVisionLoad.java
@@ -8,6 +8,11 @@ import org.usfirst.frc.team4322.commandv2.Command;
 public class DriveVisionLoad extends Command {
     private Command command;
 
+    public enum DVLStateMachine {
+        nothing,
+        loadingPanel,
+        placingPanel
+    }
     public DriveVisionLoad() {
         super(1.0);
         require(Robot.drive);
@@ -20,14 +25,27 @@ public class DriveVisionLoad extends Command {
         VisionHelper.start();
     }
 
+    /*
+    *Pseudocode:
+    *
+    * 1) Check for targets
+    *    >If targets present, execute the code
+    *    >If not, have the robot stand still and transfer control to the driver.
+    */
     @Override
     protected void execute() {
         Robot.vision.load3D();
         if (VisionHelper.hasTargets()) {
             IntakePanelFromStationKt.IntakePanelFromStation().start();
+        } else {
+
         }
     }
 
+    /*
+    * Task: Make sure that DriveVisionLoad has a state machine so that it will only start one command at a
+    * time.
+    * */
     @Override
     protected boolean isFinished() {
         return false;
