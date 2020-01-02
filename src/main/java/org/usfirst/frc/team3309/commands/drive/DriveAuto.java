a01a110b10fb7c93adb2a67f6770934a134186cf
Mark Ghebrial <mghebrialjr@icloud.com>
2020-01-02 13:27:00 -0800

    Added documentation

--- src/main/java/org/usfirst/frc/team3309/commands/drive/DriveAuto.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/DriveAuto.java
@@ -66,47 +66,58 @@ public class DriveAuto extends Command {
         double inchesTraveled = Robot.drive.encoderCountsToInches(Robot.drive.getEncoderDistance());
 
         if (nextPoint.turnRadiusInches == 0) {
-            /* Drive Straight
-             * Accelerate until the robot is 10% to its destination. Then remain at a constant
-             * speed until 90% to destination. Finally, decelerate at the same rate of acceleration
-             * until destination has been reached
+            /*
+             * Drive Straight
+             *
+             * Accelerate until the robot has traveled the acceleration distance. Then
+             * remain at a constant speed until robot enters the deceleration zone. Finally,
+             * decelerate until destination has been reached
+             *
+             *          │    ______________
+             *          │   /              \
+             * Velocity │  /                \
+             *          │ /                  \___
+             *          │/                       \
+             *           -------------------------
+             *                    Time
+             *
+             * Prepare to enter the logic jungle. You have been warned
              */
             turn = 0;
             if (state == travelState.accelerating) {
                 if (inchesTraveled < nextPoint.accelerationDistance) {
-                    if (speed <= nextPoint.maxTravelSpeedEncoderCounts) {
-                        if (inchesTraveled / nextPoint.accelerationDistance >= nextPoint.creepSpeed) {
-                            speed = inchesTraveled / nextPoint.accelerationDistance;
-                        } else {
-                            speed = nextPoint.creepSpeed;
-                        }
+                    //Prevent the robot from not moving by making the minimum speed the creepSpeed
+                    if (inchesTraveled / nextPoint.accelerationDistance >= nextPoint.creepSpeed) {
+                        speed = inchesTraveled / nextPoint.accelerationDistance;
+                    } else {
+                        speed = nextPoint.creepSpeed;
                     }
                 } else {
-                    //Change state once 10% to the destination
                     state = travelState.cruising;
                 }
             } else if (state == travelState.cruising){
                 if (inchesTraveled < inchesFromWaypoints - nextPoint.decelerationDistance) {
                     speed = nextPoint.maxTravelSpeed;
                 } else {
-                    //Change state once 90% to the destination
                     state = travelState.decelerating;
                 }
             } else if (state == travelState.decelerating){
                 if (inchesTraveled <= inchesFromWaypoints) {
+                    //Prevent the robot from not moving by making the minimum speed the creepSpeed
                     if (inchesTraveled / (inchesFromWaypoints - nextPoint.decelerationDistance) > nextPoint.creepSpeed) {
                         speed = inchesTraveled / (inchesFromWaypoints - nextPoint.decelerationDistance);
                     } else {
                         speed = nextPoint.creepSpeed;
                     }
                 } else {
-                    //Stop and go to next waypoint
+                    //Stop robot and start moving to next waypoint
                     speed = 0;
                     nextWaypointIndex++;
                     state = travelState.stopped;
                     Robot.drive.zeroEncoders();
                 }
             }
+
         } else if (nextPoint.turnRadiusInches !=0 && nextPoint.crossfieldInches + nextPoint.downfieldInches != 0) {
             state = travelState.turning;
 
