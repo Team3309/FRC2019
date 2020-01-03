7a51082c1dff2b5121d984a92de65456d5215ecb
Mark Ghebrial <mghebrialjr@icloud.com>
2020-01-02 16:24:48 -0800

    Renamed Waypoint class member variables (again)

--- src/main/java/org/usfirst/frc/team3309/commands/drive/Waypoint.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/Waypoint.java
@@ -3,27 +3,28 @@ package org.usfirst.frc.team3309.commands.drive;
 import org.usfirst.frc.team3309.Robot;
 
 public class Waypoint {
-    double downfieldInches; //how far the waypoint is from the driver station
-    double crossfieldInches; //lateral position of the waypoint
-    double turnRadiusInches; //centered on the vertices of the straight-line path, not the guide circles
-    double maxStraightTravelSpeed; //Inches per second
-    boolean reverse;  // robot backs into waypoint
+    double downfieldInches = 0; //how far the waypoint is from the driver station
+    double crossfieldInches = 0; //lateral position of the waypoint
+    double turnRadiusInches = 0; //centered on the vertices of the straight-line path, not the guide circles
+    boolean reverse = false;  // robot backs into waypoint
 
     double creepSpeed = 5; //Inches per second
 
     //TODO: Tune these
-    double linearAcelerationRateInchesPerSec2 = 0; //Inches per second^2
-    double linearDecelerationRateInchesPerSec2 = 0; //Also in inches per second^2
-    double angularAcelerationRateDegreesPerSec2 = 0;
-    double angularDcelerationRateDegreesPerSec2 = 0;
+    double maxLinearSpeed = 0; //Inches per second
+    double maxAngularSpeed = 0;
+    double linearAccelerationInchesPerSec2 = 0; //Inches per second^2
+    double linearDecelerationInchesPerSec2 = 0; //Also in inches per second^2
+    double angularAccelerationDegreesPerSec2 = 0;
+    double angularDecelerationDegreesPerSec2 = 0;
 
     double cruiseDistance = 12; //Inches
 
-    double maxTravelSpeedEncoderCounts;
-    double linearAcelerationRateEncoderCountsPerSec2;
-    double linearDecelerationRateEncoderCountsPerSec2;
-    double cruiseDistanceEncoderCounts;
-    double creepSpeedEncoderCounts;
+    double maxTravelSpeedEncoderCounts = 0;
+    double linearAccelerationEncoderCountsPerSec2 = 0;
+    double linearDecelerationEncoderCountsPerSec2 = 0;
+    double cruiseDistanceEncoderCounts = 0;
+    double creepSpeedEncoderCounts = 0;
 
     public Waypoint() {
         initialize();
@@ -44,13 +45,14 @@ public class Waypoint {
     public Waypoint(double downfieldInches,
                     double crossfieldInches,
                     double turnRadiusInches,
-                    double maxStraightTravelSpeed,
+                    double maxLinearSpeed,
+                    double maxAngularSpeed,
                     double creepSpeed,
                     boolean reverse) {
         this.downfieldInches = downfieldInches;
         this.crossfieldInches = crossfieldInches;
         this.turnRadiusInches = turnRadiusInches;
-        this.maxStraightTravelSpeed = maxStraightTravelSpeed;
+        this.maxLinearSpeed = maxLinearSpeed;
         this.creepSpeed = creepSpeed;
         this.reverse = reverse;
 
@@ -58,9 +60,9 @@ public class Waypoint {
     }
 
     private void initialize () {
-        maxTravelSpeedEncoderCounts = Robot.drive.inchesPerSecondToEncoderVelocity(maxStraightTravelSpeed);
-        linearAcelerationRateEncoderCountsPerSec2 = Robot.drive.inchesPerSecondToEncoderVelocity(linearAcelerationRateInchesPerSec2);
-        linearDecelerationRateEncoderCountsPerSec2 = Robot.drive.inchesPerSecondToEncoderVelocity(linearDecelerationRateInchesPerSec2);
+        maxTravelSpeedEncoderCounts = Robot.drive.inchesPerSecondToEncoderVelocity(maxLinearSpeed);
+        linearAccelerationEncoderCountsPerSec2 = Robot.drive.inchesPerSecondToEncoderVelocity(linearAccelerationInchesPerSec2);
+        linearDecelerationEncoderCountsPerSec2 = Robot.drive.inchesPerSecondToEncoderVelocity(linearDecelerationInchesPerSec2);
         cruiseDistanceEncoderCounts = Robot.drive.inchesToEncoderCounts(cruiseDistance);
         creepSpeedEncoderCounts = Robot.drive.inchesPerSecondToEncoderVelocity(creepSpeed);
     }
